2957fb80e7bee1c4b2931110edad12abff6512eb
Jacob <jacobsolano1988@gmail.com>
2023-01-12 15:38:39 -0600

    Voice working but not linked to menu.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
+import React, { useState } from 'react';
 import TextInputField from './SpeakBox/text-input-field';
 import VoiceList from './SpeakBox/voice-list';
 
 
-function App() {
+const App = () => {
+  const [voice, setVoice] = useState('');
+
   return (
     <div>
-        <TextInputField />
-        <VoiceList />
+      <VoiceList setVoice={setVoice} />
+      <TextInputField selectedVoice={voice} />
     </div>
-)
-;
-}
+  );
+};
 
 export default App;
--- src/SpeakBox/text-input-field.js
+++ src/SpeakBox/text-input-field.js
@@ -1,19 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
-const TextInputField = () => {
+const TextInputField = (props) => {
   const [text, setText] = useState('');
+  const { selectedVoice } = props;
 
   const handleChange = (event) => {
-    setText(event.target.value);
+    if (event.keyCode === 13) {
+        const synth = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.voice = selectedVoice;
+        synth.speak(utterance);
+        setText('');
+    } else {
+        setText(event.target.value);
+    }
   };
 
   useEffect(() => {
     // some logic here that uses the text variable
   }, [text]);
 
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 13) {
+      speak();
+      setText('');
+    }
+  };
+
+  const speak = () => {
+    if (text !== '') {
+      const speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+    }
+  };
+
   return (
     <div>
-      <input type="text" value={text} onChange={handleChange} />
+      <input
+        type="text"
+        value={text}   
+        onKeyDown={handleKeyPress}
+        onChange={handleChange}
+     
+        placeholder="Say Something!"
+      />
     </div>
   );
 };
